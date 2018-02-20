ce9531f955c65ba154dd0a44d8b353dc638c4b59
fabien barthelot <fabien.barthelot@gmail.com>
2018-02-20 16:22:57 +0100

    first commit

--- public/js/connexion.js
+++ public/js/connexion.js
@@ -1,9 +1,9 @@
-var mysql = require('mysql');
+/*var mysql = require('mysql');
 var net = require('net');
 
 var con= mysql.createConnection({
 
-    host:'localhost',
+    host:'172.17.0.2',
     user:'root',
     password:'poec',
     database:'dbProfile'
@@ -22,67 +22,5 @@ con.connect(function(err){
         console.log("result"+result)
     });
 
-});
+});*/
 
-
-/*    config = require('./mySQLConfig.js'),
-    tunnel = require('tunnel-ssh');*/
-
-/*
-var connection = module.exports = function(){};
-*/
-
-/*
-createDBConnection = function(){
-
-
-    var mysqlConnection = mysql.createConnection({
-
-        host: config.databaseConfig.host,
-        user: config.databaseConfig.username,
-        password: config.databaseConfig.password,
-        database: config.databaseConfig.database,
-        connectTimeout: config.databaseConfig.timeout
-    });
-
-    return mysqlConnection;
-};*/
-//
-//connection.invokeQuery = function(sqlQuery){
-//
-//    var data = undefined;
-//
-////    var sshTunnel = tunnel(config.sshTunnelConfig, function(error, result) {
-//
-//        var sqlConnection = createDBConnection();
-//
-//        sqlConnection.connect(function (err) {
-//            console.log(err.code);
-//        });
-//        sqlConnection.on('error', function (err) {
-//            console.log(err.code);
-//        });
-//
-//        data = sqlConnection.query({
-//            sql: sqlQuery,
-//            timeout: config.mySQLConfig.timeout
-//        }, function (err, rows) {
-//
-//            if (err && err.code) {
-//                console.log("error code: " + err.code)
-//            }
-//            if (err) {
-//                console.error("error stack: " + err.stack)
-//            }
-//            ;
-//
-//            if (rows) {
-//                console.log("We have Rows!!!!!!")
-//            }
-//        });
-//
-//        sqlConnection.destroy();
-////    });
-//
-//    return data;
-//};
